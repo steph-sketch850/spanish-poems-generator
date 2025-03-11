3ac7f16eb2752fff187a71e75f9128f546b4b282
steph-sketch850 <145380074+steph-sketch850@users.noreply.github.com>
2025-03-11 19:17:14 -0400

    Fixed spelling and naming issues

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Generated poem");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,14 +12,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "268bao3528f57224ftf9c00a13f8a465";
-  let prompt = `User instructions: Generate a poem in Spanish ${instructionsInput.value}`;
   let context =
-    "You are an adventurous poem writer and love to write short poems. You're mission is to generate a four line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Do not include title with poem and end with a <strong> element 'SheCodes AI' in the end, not at the beginning.";
+    "You are an adventurous poet and love to write short poems. Your mission is to generate a 4 line Spanish poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem, not at the beginning. The poem should be about the user instructions.";
+  let prompt = `User instructions: Generate a Spanish poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
